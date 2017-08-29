2291292245533a83f1d53cdfcebc92028e611df8
Chelito Manayon <brochelito@gmail.com>
2017-08-29 23:46:34 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import App from './components/App';
+
+    ReactDOM.render(
+      <App initialContest={window.initialData.contests} />,
+      document.getElementById('root')
+    );
+    
+
+  
+
+
+
